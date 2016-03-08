f902f48a559d842ca6254bac835d5e4d60a3b05e
Alf Bjørn <abhustoft@gmail.com>
2016-03-08 19:42:33 +0100

    Path error fix

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -6,7 +6,7 @@ import {Hero} from './hero';
 @Component({
     selector: 'my-hero-detail',
     templateUrl: './app/hero-detail.component.html',
-    styleUrls: ['hero-detail.component.css'],
+    styleUrls: ['./app/hero-detail.component.css'],
     inputs: ['hero']
 })
 export class HeroDetailComponent {
