0e84472e90921028081467a731d15ac61852a9c8
Alf Bjørn <abhustoft@gmail.com>
2016-03-12 18:14:43 +0100

    Started on http in bottle-service

--- app/bottle.service/bottle.service.ts
+++ app/bottle.service/bottle.service.ts
@@ -1,10 +1,29 @@
 import {Bottle} from './../bottle/bottle';
 import {BOTTLES} from './../mock-bottles/mock-bottles';
 import {Injectable} from 'angular2/core';
+import {Http, Headers} from 'angular2/http';
+
+//Check:
+// https://auth0.com/blog/2015/10/15/angular-2-series-part-3-using-http/
 
 @Injectable()
 export class BottleService {
+
+    getVP() {
+        //fetchUrl('http://104.236.28.133:8080/prices/list')
+        //    .then(function(response) {
+        //        return response.json()
+        //    }).then(function(json) {
+        //    console.log('parsed json', json)
+        //}).catch(function(ex) {
+        //    console.log('parsing failed', ex)
+        //})
+
+    }
+
+
     getBottles() {
+        this.getVP();
         return Promise.resolve(BOTTLES);
     }
     // See the "Take it slow" appendix
